89c5731e24592f2edab39f73731833024f7640af
alex <sharpiron@mail.ru>
2023-08-06 23:05:44 +0500

    new commit

---
+++ base/driver_manager.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+class DriverManager:
+    def __init__(self, base_url):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
+        self.service = Service()
+        self.driver = webdriver.Chrome(options=self.options, service=self.service)
+        self.driver.maximize_window()
+        self.driver.get(base_url)
