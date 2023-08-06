4a43294b11644150e0dc9f3f4b850f8b0c2fa647
alex <sharpiron@mail.ru>
2023-08-06 22:48:21 +0500

    search implemented

---
+++ pages/product_selection.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class 
